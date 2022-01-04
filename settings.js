e3748731e3c2fb747e8cc848b5de2bad7af680fd
K.Kimura <dotnsf@gmail.com>
2022-01-04 15:30:03 +0900

    package.json propery configured.

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
-exports.mysql_hostname = '127.0.0.1';
+exports.mysql_hostname = '';
 exports.mysql_port = 3306;
 exports.mysql_database = 'mydb';
-exports.mysql_username = 'user1';
-exports.mysql_password = 'pass1';
+exports.mysql_username = '';
+exports.mysql_password = '';
